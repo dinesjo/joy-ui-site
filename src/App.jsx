35322dde57b24160fbfccc709d8517940f768c09
dinesjo <dinesjo@kth.se>
2023-09-09 16:29:54 +0200

    refactor: new Node class

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,37 @@ import {
   useColorScheme,
 } from "@mui/joy";
 import { useEffect, useRef, useState } from "react";
-import { FaMoon, FaSun } from "react-icons/fa";
+import { FaBorderAll, FaMoon, FaSun } from "react-icons/fa";
+import Node from "./Node";
 
 const colorMap = {
   light: {
     obstacle: "#222",
     grid: "#e0e0e0",
+    start: "#12467B",
+    end: "#A51818",
   },
   dark: {
     obstacle: "#e0e0e0",
-    grid: "#424242",
+    grid: "#333",
+    start: "#12467B",
+    end: "#A51818",
   },
 };
 
 export const Canvas = (props) => {
-  const { gridSize, grid, setGrid, theme } = props;
+  const {
+    gridSize,
+    grid,
+    setGrid,
+    theme,
+    startNode,
+    setStartNode,
+    endNode,
+    setEndNode,
+  } = props;
   const canvasRef = useRef(null); // Reference to the canvas element
+  // const [draggingNode, setDraggingNode] = useState(); // Whether the user is dragging the start/end node
 
   // Initialize the grid
   useEffect(() => {
@@ -31,15 +46,14 @@ export const Canvas = (props) => {
     for (let row = 0; row < gridSize; row++) {
       const currentRow = [];
       for (let col = 0; col < gridSize; col++) {
-        currentRow.push({
-          x: col,
-          y: row,
-          isObstacle: false,
-          animationFrame: 0,
-        });
+        currentRow.push(new Node(col, row));
       }
       grid.push(currentRow);
     }
+    // Add the start and end nodes
+    grid[startNode.row][startNode.col].type = "start";
+    grid[endNode.row][endNode.col].type = "end";
+
     setGrid(grid);
   }, [gridSize]);
 
@@ -48,6 +62,7 @@ export const Canvas = (props) => {
     // Get the canvas context
     const canvas = canvasRef.current;
     const context = canvasRef.current.getContext("2d");
+    const cellSize = context.canvas.width / gridSize;
 
     // Clear the canvas
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
@@ -58,12 +73,22 @@ export const Canvas = (props) => {
     // Draw grid from the grid state
     grid.forEach((row) => {
       row.forEach((cell) => {
-        const cellSize = context.canvas.width / gridSize;
-        const x = cell.x * cellSize;
-        const y = cell.y * cellSize;
-        context.strokeRect(x, y, cellSize, cellSize);
-        if (cell.isObstacle) {
+        const x = cell.col * cellSize;
+        const y = cell.row * cellSize;
+        context.strokeRect(x, y, cellSize, cellSize); // Draw the cell
+        if (cell.type === "obstacle") {
+          // Draw the obstacle
           context.fillStyle = colorMap[theme].obstacle;
+          context.fillRect(x, y, cellSize, cellSize);
+        }
+        if (cell.type === "start") {
+          // Draw the start node
+          context.fillStyle = colorMap[theme].start;
+          context.fillRect(x, y, cellSize, cellSize);
+        }
+        if (cell.type === "end") {
+          // Draw the end node
+          context.fillStyle = colorMap[theme].end;
           context.fillRect(x, y, cellSize, cellSize);
         }
       });
@@ -84,17 +109,17 @@ export const Canvas = (props) => {
 
   const handleMouseDown = (e) => {
     const canvas = canvasRef.current;
-    // Calculate the cell coordinates where the user clicked
     const cellSize = canvas.width / gridSize;
+    // Calculate the cell coordinates where the user clicked
     const row = Math.floor(e.offsetY / cellSize);
     const col = Math.floor(e.offsetX / cellSize);
 
-    // Check if the cell is not already an obstacle
+    // Check if the cell is empty
     const cell = grid[row][col];
-    if (!cell.isObstacle) {
+    if (cell.type === "empty") {
       setGrid((prevGrid) => {
         const newGrid = [...prevGrid];
-        newGrid[row][col] = { ...cell, isObstacle: true };
+        newGrid[row][col] = { ...cell, type: "obstacle" };
         return newGrid;
       });
     }
@@ -165,13 +190,18 @@ const sliderMarks = [
 
 const defGridSize = 20;
 function App() {
+  // Initialize the grid size from local storage
   const [gridSize, setGridSize] = useState(
     parseInt(localStorage.getItem("gridSize")) || defGridSize
   );
   useEffect(() => {
     localStorage.setItem("gridSize", gridSize);
   }, [gridSize]);
+  // Initialize the grid
   const [grid, setGrid] = useState([]); // Initialize as an empty grid
+  // Initialize start/end nodes
+  const [startNode, setStartNode] = useState(new Node(0, 0, "start"));
+  const [endNode, setEndNode] = useState(new Node(10, 10, "end"));
 
   return (
     <>
@@ -179,11 +209,14 @@ function App() {
         direction="row"
         justifyContent="center"
         alignItems="center"
-        spacing={10}
+        spacing={6}
         sx={{ width: "100vw", px: 2, py: 2.5 }}
       >
         <Box sx={{ width: 250 }}>
-          <Typography sx={{ textAlign: "center" }}>
+          <Typography
+            startDecorator={<FaBorderAll />}
+            sx={{ textAlign: "center" }}
+          >
             Grid Size [{gridSize}x{gridSize}]
           </Typography>
           <Slider
@@ -193,7 +226,6 @@ function App() {
             min={20}
             max={100}
             marks={sliderMarks}
-            sx={{ mx: 4 }}
             onChange={(e, v) => {
               setGridSize(v);
             }}
@@ -207,7 +239,9 @@ function App() {
               const newGrid = [...prevGrid];
               newGrid.forEach((row) => {
                 row.forEach((cell) => {
-                  cell.isObstacle = false;
+                  if (cell.type === "obstacle") {
+                    cell.type = "empty";
+                  }
                 });
               });
               return newGrid;
@@ -231,6 +265,10 @@ function App() {
           width={parent.innerWidth}
           height={parent.innerHeight - 120}
           theme={useColorScheme().mode}
+          startNode={startNode}
+          setStartNode={setStartNode}
+          endNode={endNode}
+          setEndNode={setEndNode}
         />
       </Sheet>
     </>
---
+++ src/Node.js
@@ -0,0 +1,13 @@
+/**
+ * Node class that represents a node in the grid
+ * @param {number} row - The row of the node
+ * @param {number} col - The column of the node
+ * @param {string} type - The type of the node
+ */
+export default class Node {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+    this.type = "none";
+  }
+}
