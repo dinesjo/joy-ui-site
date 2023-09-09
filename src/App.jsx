adec9b5cced8d0bc3af3129188d47630955da830
dinesjo <dinesjo@kth.se>
2023-09-09 11:35:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Button, Sheet, Typography, useColorScheme } from '@mui/joy';
+import * as React from 'react';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  // necessary for server-side rendering
+  // because mode is undefined on the server
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="soft"
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
+
+
+function App() {
+  return (
+    <>
+      <ModeToggle />
+      <Sheet
+        sx={{
+          width: 300,
+          mx: "auto", // margin left & right
+          my: 4, // margin top & bottom
+          py: 3, // padding top & bottom
+          px: 2, // padding left & right
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          borderRadius: "sm",
+          boxShadow: "md",
+        }}
+      >
+        Hello World!
+      </Sheet>
+    </>
+  );
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import "@fontsource/inter";
+import { CssBaseline, CssVarsProvider } from '@mui/joy';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CssVarsProvider>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>,
+)
